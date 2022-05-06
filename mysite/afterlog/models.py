f72635df6ef20cf3b781b1ecc0a1f0f2bc6f0e95
Emilia Marchese <emiami00@gmail.com>
2022-05-06 17:24:10 +0300

    Cleanup and comments for faults

--- mysite/afterlog/models.py
+++ mysite/afterlog/models.py
@@ -8,12 +8,9 @@ class Account(models.Model):
     balance = models.IntegerField()
 
 class Message(models.Model):
-    #source = models.ForeignKey(User, on_delete=models.CASCADE, related_name='source')
-    #target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
     source=models.TextField()
     target = models.TextField()
     amount = models.TextField()
-    #time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
 def user_directory_path(instance, filename):
@@ -21,8 +18,7 @@ def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.owner.id, filename)
 
 class Coupon(models.Model):
-	#owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    owner = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     data = models.FileField(upload_to=user_directory_path)
 
 class Mail(models.Model):
--- mysite/afterlog/views.py
+++ mysite/afterlog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.db import transaction
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Account, Message, Coupon, Mail
@@ -13,9 +13,10 @@ from io import StringIO
 from django.db import connection
 
 
-# Fault: the download view for the coupons does not check whether the accessing user is the same one that is logged in (hihupload)
-# Login as bob, localhost/download/1 -> downloads alice's coupon (assumed you know the id of alice's coupon, or brute force)
 
+# F1: Broken Access Control: the download view for the coupon does not check whether the accessing user is the same of as the logged user
+# Replicate F1: Login as bob, navigate to download/1 -> You are able to download Alice's coupon 
+# (assuming you added a text file in Coupon account using the admin django website under Alice's name). This also assumes that bob knows alice's coupon id (1) (e.g. could have gotten it by brute force) 
 @login_required
 def downloadView(request, couponid):
     c = Coupon.objects.get(pk=couponid)
@@ -25,8 +26,7 @@ def downloadView(request, couponid):
 
     return response
 
-# Fix of fault:
-
+# F1 fix: Broken Access Control:
 # @login_required
 # def downloadView(request, couponid):
 #     c = Coupon.objects.get(pk=couponid)
@@ -38,6 +38,7 @@ def downloadView(request, couponid):
 #     else:
 #         return redirect('/')
 
+
 # This code is taken from part3-16.xss and is only used to check if the stolen cookie is there (if you want). Go to /mail to see
 @csrf_exempt 
 def mailView(request):
@@ -46,10 +47,23 @@ def mailView(request):
 	return HttpResponse('')
 
 
-# Faults: 
-# SQL injections
-# Using get parameters make it suscitible to crsf
-# 
+# Fault 2: Insecure design. The transactionView does not check whether there are enough money for the trasaction in the source account 
+# Replicate F2: Log in as bob and send money to alice that is greater than bob's balance. The transaction should be allowed and bob's balance should now be negative
+
+# Fault 4: XSS
+# Replicate F4: log in a bob, and insert the message present in msg.html into the message box. Send the message to alice, you should see the crfs cookie appear
+# printed in the command line (or navigate do dev tools -> network -> payload)
+
+# Fault 5: crfs
+# Replicate F5: bob is logged in in his account and receives a link from alice. The link is the following: http://127.0.0.1:8000/transfer/?to=alice&amount=30&content=hahahaa 
+# (you can try and insert that when you are logged in as bob). This immediately transfers the money to alice. 
+
+# Fix F5: crfs
+# Uncomment crfs line in afterlog.html at line 68 and uncommed crfs required line at line 67 before transferView. Now you can try to insert the url http://127.0.0.1:8000/transfer/?to=alice&amount=30&content=hahahaa
+# when you are logged in as bob and you will see nothing happens because you did not input the crfs token in the url (which is now required)
+
+
+#@requires_csrf_token
 @login_required
 def transferView(request):
     s = request.user
@@ -59,26 +73,44 @@ def transferView(request):
     with transaction.atomic():
         acc1 = Account.objects.get(user=s)
         acc2 = Account.objects.get(user=t)
-        ## Possibly add improper check up on balance here
-        #Message.objects.create(source=, target=target, amount = amount, content= content)
-        #conn = sqlite3.connect('db.sqlite3')
-        #cursor = conn.cursor()
-        #response = cursor.execute("INSERT INTO Message (source, target, amount, time, content) VALUES (%s, %s, %s, %s, %s)" % (source,target, amount, time, content)).fetchall()
+        
         sourcename = s.username
         targetname = t.username
         Message.objects.create(source=sourcename, target=targetname, amount = str(amount), content= content)
 
-        #newmess = Message.objects.raw("INSERT INTO Message (source, target, amount, content) VALUES (%s,%s,%s,%s)" % (sourcename , targetname, str(amount), content))
-        #cursor = connection.cursor()
-        #cursor.executescript("INSERT INTO afterlog_message (source, target, amount, content) VALUES (%s, %s, %s, %s)" % (sourcename,targetname, str(amount),content))
-        #amountb = str(amount)
-        #ursor.executescript("INSERT INTO afterlog_message (source, target, amount, content) VALUES ('%s', '%s', '%s', '%s')" % (sourcename,targetname, amountb ,content))
-        #cursor.close()
         acc1.balance -= amount
         acc2.balance += amount
         acc1.save()
         acc2.save()
     return redirect('/')
+
+# Fix F2: Insercure design
+# @login_required
+# def transferView(request):
+#     s = request.user
+#     t = User.objects.get(username = request.GET.get('to'))
+#     amount = int(request.GET.get('amount'))
+#     content = request.GET.get('content')
+#     with transaction.atomic():
+#         acc1 = Account.objects.get(user=s)
+#         acc2 = Account.objects.get(user=t)
+#         if acc1.balance > amount:
+#             sourcename = s.username
+#             targetname = t.username
+#             Message.objects.create(source=sourcename, target=targetname, amount = str(amount), content= content)
+
+#             acc1.balance -= amount
+#             acc2.balance += amount
+#             acc1.save()
+#             acc2.save()
+#     return redirect('/')
+
+
+# Fix F4: XSS
+# Sanitize the message of all < > so that the javascript cannot be executed, or even better use some safe library that sanitizes the text for you, 
+# for example urllib.parse.quote() function
+
+
 
 
 def convertTuple(t):
@@ -88,8 +120,10 @@ def convertTuple(t):
     return s
 
 
-# Fault: SQL injection: go to http://127.0.0.1:8000/aggregate/?from=0%27%20UNION%20SELECT%20password%20FROM%20auth_user%20WHERE%20username%20LIKE%20%27admin
-# ?from=0' UNION SELECT password FROM auth_user WHERE username LIKE 'admin
+# Fault 3: SQL injection, the user can insert a query that returns passowords 
+# Replicate F3: login as bob, and go to aggregate/?from=0' UNION SELECT password FROM auth_user WHERE username LIKE 'admin
+# You should be redirected to a new page where the admin passowrd shouls up
+# Note that the malicious query is the following: ?from=0' UNION SELECT password FROM auth_user WHERE username LIKE 'admin
 def aggregateView(request):
     sname = request.user.username
     tname = request.GET.get('from')
@@ -103,16 +137,24 @@ def aggregateView(request):
         response.write("<p>" + r + ".<p>")
     return response
 
+# Fix F3: SQL injection
+# This removes the ambigous c.execute() function that allows UNION script, and uses the default django models.get()
+# def aggregateView(request):
+#     sname = request.user.username
+#     tname = request.GET.get('from')
+#     messages = Message.objects.filter(source = sname, target = tname)
+#     response = HttpResponse()
+#     for message in messages: 
+#         #print(message.content)
+#         response.write("<p>" + message.content + ".<p>")
+#     return response
+
+
+
+
 
 @login_required
 def homePageView(request):
-    # if request.method == 'POST':
-    #     sender = request.user
-    #     receiver = User.objects.get(username = request.POST.get('to'))
-    #     amount = int(request.POST.get('amount'))
-    #     message = request.POST.get('content')
-    #     transfer(sender, receiver, amount, message)
-
     accounts = Account.objects.exclude(user_id=request.user.id)
     messages = Message.objects.filter(source = request.user.username)
     cs = Coupon.objects.filter(owner=request.user.id)
@@ -121,29 +163,23 @@ def homePageView(request):
     return render(request, 'afterlog.html', context)
 
 
-    # Fault 1: Insecure design: does not check money in account, user can send money to themselves
-    # Fault 2: Broken Access Control: does not check whether the owner is the logged user when downloading coupons
-    # Fault 3: XSS, fix using sanitization
-    # Fault 4: Sql injection using message (cursor instead of model)
-    #INSERT INTO Message (source, target, amount, time, content) VALUES (source, target, amount, time, content);
-    #content = "5') UNION SELECT password FROM Users WHERE admin LIKE '1";
-    # Fault 5: crsf: GET request for transactions -> transactions can be seen in the server log
-    # CRSF token is missing in the transaction api -> Attacker can send special url to the victim (e.g. http://.../transaction?target=eve&amount=1000000&message=hahaha)
-
-
-    # Why is it that when I do runserver it goes already in the logged in page
-    #crsf: bob is logged in and he receives a text message from alice with link http://127.0.0.1:8000/transfer/?to=alice&amount=30&content=hahahaa
-
-    # Other fault: you can also send money to yourself. Login as bob, and run /transfer/?from = bob etc. Fix: add checking that accounts are different
-    # xss fault: write msg.html to message, crf cookies appears printed in command line or dev tool network, payload
-
-    # Novel: send heart, with sql injection
 
 
     #SET UP
 
-    # Crete admin: manage.py create superuser (admin, admin)
-    # Users: go to the admin login page and manually insert
-    # Coupon: same, upload a text file
-    # Accounts: same
+    # Crete admin: manage.py create superuser (name: admin, password: admin)
+    # Users: go to the admin login page (admin/login/?next=/admin/) and manually insert: 
+    # 1) Users: 
+    # - username: bob, password: squarepants
+    # - username: alice, password: redqueen
+    # 2) Accounts: 
+    # - user: bob, balance: 50
+    # - user: alice, balance: 300
+    # 3) Coupon: 
+    # - owner: alice
+    # - data: insert file "coupon.txt" in repository (or create your own text file to upload)
 
+    # Now exit the server, go to mysite directory and run: 
+    # 1) python3 manage.py makemigrations
+    # 2) python3 manage.py migrate
+    # You can restart the server and proceed with testing the faults. 
